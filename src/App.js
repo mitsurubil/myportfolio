5a4715181ac35ba9267d03b576a13aa627e16306
mitsurubil <mitsurubil@hotmail.com>
2019-08-20 18:30:49 +0100

    Test commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <img src={Bg} alt='Laptop with Matrix background' />
       <div className='text-block'>
         <h1>Bilal Nehiri</h1>
         <p>London based Full-Stack Web-Developer</p>
       </div>
-      <img src={Bg} alt='Laptop with Matrix background' />
+      <div className='aboutme'>
+        <h1>About me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <p>ReactJs</p>
+        <p>HTML5</p>
+        <p>CSS3</p>
+      </div>
     </div>
   );
 }
