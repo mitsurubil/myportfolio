e09102f6945e74d1785175d6bc591fa8782f5691
mitsurubil <mitsurubil@hotmail.com>
2019-08-23 14:46:12 +0100

    Updates

---
+++ precache-manifest.d228a79fd7c6e4663fce31fbf729980a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a92c732b38c8a953d6c65d1627a6143",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "0af8abb2000d62d1ea89",
+    "url": "/myportfolio/static/css/main.fa9df1ae.chunk.css"
+  },
+  {
+    "revision": "3e8eed92e64772f82e3f",
+    "url": "/myportfolio/static/js/2.e7534676.chunk.js"
+  },
+  {
+    "revision": "0af8abb2000d62d1ea89",
+    "url": "/myportfolio/static/js/main.f3c0898d.chunk.js"
+  },
+  {
+    "revision": "4ba35835c09a2b0f1a6d",
+    "url": "/myportfolio/static/js/runtime~main.2ecbf68a.js"
+  },
+  {
+    "revision": "391edb836c864850728a70229e7beebc",
+    "url": "/myportfolio/static/media/bg-hp.391edb83.jpg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/myportfolio/static/media/css3-icon.2db97d69.svg"
+  },
+  {
+    "revision": "17946deef33a20ef31e585c48c82fbb4",
+    "url": "/myportfolio/static/media/database-icon.17946dee.svg"
+  },
+  {
+    "revision": "7df930e914aeb075406ac61c55514255",
+    "url": "/myportfolio/static/media/heroku-icon.7df930e9.svg"
+  },
+  {
+    "revision": "54de15268b387010a4116fa31b58c284",
+    "url": "/myportfolio/static/media/html5-icon.54de1526.svg"
+  },
+  {
+    "revision": "e169ba293cabaff0dc96397aed47a250",
+    "url": "/myportfolio/static/media/javascript-icon.e169ba29.svg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/myportfolio/static/media/nodejs-icon.be2df796.svg"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/myportfolio/static/media/react-icon.eab76719.svg"
+  }
+]);
