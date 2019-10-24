9fa5924eb0d171d6c34bfc04820e99a980f659b2
mitsurubil <mitsurubil@hotmail.com>
2019-10-24 16:53:54 +0100

    Added a contact link

--- src/background.js
+++ src/background.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Bg from './images/bg-hp.jpg';
-import Bgmatrix from './images/bg-hp-matrix.jpg';
+import video from './videos/video.mp4';
+
 
 export default function Background() {
     return (
         <div className='container1'>
+            {/* <video autoPlay loop muted>
+                <source src={video} type='video/mp4' className='item' />
+            </video> */}
             <img src={Bg} alt='Black laptop turned on' className='item' />
             <div className='text-block'>
                 <h1>Bilal Nehiri</h1>
-                <p>Full Stack Web Developer</p>
+                <p>Junior Full Stack Web Developer</p>
+                <a href='mailto: bilal.nehiri@gmail.com'><button>CONTACT ME!</button></a>
+                <div className='socials'>
+                    <i class="fa fa-github" ></i>
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                </div>
             </div>
         </div>
     );
