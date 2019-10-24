afa94043e7041da31fb68b71e39e1695afd411bb
mitsurubil <mitsurubil@hotmail.com>
2019-10-24 17:04:30 +0100

    Typo correction in Project section

--- src/projects.js
+++ src/projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
   return (
     <div className='projects' id='projects'>
       <h1>Projects</h1>
-      <p>Since Practice makes perfect, here are several projects I did through the Kodiri Bootcamp I attended or simply because I was curious. Please click on each desription to be redirected to the working app.</p>
+      <p>Since practice makes perfect, here are several projects I did through the Kodiri Bootcamp I attended or simply because I was curious. Please click on each desription to be redirected to the working app.</p>
       <div className='container3'>
         {
           getProjects().map(project => (
